32f3032a5b64bbf867159bd889f612d8eaa82a55
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-10-07 07:59:20 +0800

    parallel route is issued

---
+++ app/dashboard/default.tsx
@@ -0,0 +1,3 @@
+export default function Default(){
+    return <p>Iam default of parent</p>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return <div>
+        Hello Iam children
+    </div>
+}
